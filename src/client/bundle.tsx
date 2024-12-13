1f76edf7b0ebba1ed703732a15be6db9523eaebe
u7chan <34462401+u7chan@users.noreply.github.com>
2024-12-14 00:18:13 +0900

    clean

--- src/client/bundle.tsx
+++ src/client/bundle.tsx
@@ -3,14 +3,12 @@ import Counter from './ui/Counter'
 
 function App() {
   return (
-    <html lang='ja'>
-      <body>
-        <h1 className='bg-gray-300 text-center font-bold text-3xl'>Hello Hono!</h1>
-        <div className='mt-2 grid place-items-center gap-2'>
-          <Counter />
-        </div>
-      </body>
-    </html>
+    <>
+      <h1 className='bg-gray-300 text-center font-bold text-3xl'>Hello Hono!</h1>
+      <div className='mt-2 grid place-items-center gap-2'>
+        <Counter />
+      </div>
+    </>
   )
 }
 
